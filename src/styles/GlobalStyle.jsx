6e0d2cdad10598c6b383d5e0ea46a5e66a1ee58d
Jay <sinaelj@gmail.com>
2023-06-13 11:32:06 +0900

    🌈 style: 전역 스타일 추가

---
+++ src/styles/GlobalStyle.jsx
@@ -0,0 +1,124 @@
+import { createGlobalStyle } from "styled-components";
+import normalize from "styled-normalize";
+
+const GlobalStyle = createGlobalStyle`
+${normalize}
+
+  :root {
+  /* Primary */
+  --color-primary: #4CA2DA;
+  --color-secondary: #7C9CB0;
+  --color-tertiary: #F2F7FA;
+
+  /* Semantic Color */
+  --color-success: #33D486;
+  --color-warn: #FFC73C;
+  --color-error: #FF5B45;
+
+  /* Black & White */
+  --color-black: #000;
+  --color-white: #fff;
+
+  /* Text Color */
+  --color-gray-2: #222;
+  --color-gray-46: #464646;
+  --color-gray-76: #767676;
+
+  /* Border & Background Color */
+  --color-dark: #b1b1b1;
+  --color-light: #D5D5D5;
+  --color-bg: #f8f8f8;
+
+  /* font-size */
+  --font-size-micro: clamp(10px, 1vw, 12px);
+  --font-size-xs: clamp(12px, 1.5vw, 14px);
+  --font-size-sm: clamp(14px, 2vw, 16px);
+  --font-size-md: clamp(15px, 2vw, 17px);
+  --font-size-lg: clamp(16px, 2vw, 18px);
+  --font-size-xl: clamp(18px, 2vw, 20px);
+  --font-size-xxl: clamp(22px, 2.5vw, 26px);
+  --font-size-title: clamp(32px, 3vw, 36px);
+  }
+
+  * {
+    box-sizing: border-box;
+  }
+
+  /* test */
+  body {
+    max-width:520px;
+    margin: auto;
+    padding: 0 16px;
+    font-family: 'LINE Seed KR' sans-serif;
+    line-height:1.5;
+    letter-spacing: -0.015rem;
+  }
+
+  a {
+  color: inherit;
+  text-decoration: none;
+  }
+
+  h1 {
+      margin: 0;
+  }
+
+  input, button {
+    background-color: transparent;
+    border: none;
+    outline: none;
+    font-family: var(--font);
+    padding: 0;
+    margin: 0;
+    cursor: pointer;
+  }
+
+  button {
+  cursor: pointer;
+  font:inherit;
+  }
+
+  button:focus-visible{
+	outline: #000 solid 2px;
+}
+
+  textarea {
+    font-family: inherit;
+    padding: 0;
+    margin: 0;
+  }
+
+  ::placeholder {
+    font-family: inherit;
+  }
+
+  label {
+    display: block;
+  }
+
+  ol, ul, li {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+  }
+
+  img{
+    max-width:100%;
+    height:auto;
+    vertical-align: top;
+  }
+
+  input:-webkit-autofill {
+    -webkit-box-shadow: 0 0 0 30px #fff inset ;
+    -webkit-text-fill-color: #000;
+  }
+
+  input:-webkit-autofill,
+  input:-webkit-autofill:hover,
+  input:-webkit-autofill:focus,
+  input:-webkit-autofill:active {
+    transition: background-color 5000s ease-in-out 0s;
+  }
+`;
+
+export default GlobalStyle;
