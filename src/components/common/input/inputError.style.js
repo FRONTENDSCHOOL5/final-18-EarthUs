768c882fbb4625f18d43e043ef6cdfeb07565142
Planting12 <simej1214@gmail.com>
2023-06-15 11:50:03 +0900

    🌈 style: error 아이콘 filter 적용

--- src/components/common/input/inputError.style.js
+++ src/components/common/input/inputError.style.js
@@ -8,4 +8,9 @@ export const ErrorTextStyle = styled.span`
 
 export const ErrorImg = styled.img`
   margin-right: 8px;
+
+  &.errorIcon {
+    filter: invert(65%) sepia(40%) saturate(7147%) hue-rotate(331deg)
+      brightness(100%) contrast(102%);
+  }
 `;
