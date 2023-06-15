877711aed4ac882b1f7931cd0f140cae1d873ba6
Planting12 <simej1214@gmail.com>
2023-06-15 11:19:23 +0900

    🌈 style: input 컴포넌트 스타일 적용

---
+++ src/components/common/input/input.style.js
@@ -0,0 +1,33 @@
+import styled from "styled-components";
+
+export const StyledLabel = styled.label`
+  font-family: "LINE Seed KR";
+  font-size: var(--font-size-xs);
+  font-weight: 700;
+  color: var(--color-gray-76);
+`;
+
+export const StyledInput = styled.input`
+  display: flex;
+  width: 488px;
+  height: 52px;
+  color: var(--color-black);
+  border-bottom: solid var(--color-dark) 2px;
+  margin-bottom: 24px;
+
+  ::placeholder {
+    color: var(--color-dark);
+    font-family: "LINE Seed KR";
+    font-size: var(--font-size-md);
+  }
+
+  // 포커스를 주었을 때
+  &:valid {
+    border-bottom-color: var(--color-primary);
+  }
+
+  // 에러 클래스가 되었을 때
+  &.error {
+    border-bottom-color: var(--color-error);
+  }
+`;
---
+++ src/components/common/input/inputError.style.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const ErrorTextStyle = styled.span`
+  display: block;
+  font-size: 15px;
+  color: var(--color-error);
+`;
+
+export const ErrorImg = styled.img`
+  margin-right: 8px;
+`;
