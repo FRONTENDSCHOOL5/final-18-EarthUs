147f07fb0eb0b17ca9499c5a02c6e14aa3cae46b
jisooyoon <yoonmallang@gmail.com>
2023-06-19 03:22:55 +0900

    🦄 refactor: 라우터 수정 및 주석 작성

--- src/components/common/header/Header.jsx
+++ src/components/common/header/Header.jsx
@@ -16,11 +16,13 @@ import { Headers, SearchInput } from "./header.style";
 export default function Header() {
   const navigate = useNavigate();
   const { pathname } = useLocation();
-  const { account } = useParams();
+  const { account, productId } = useParams();
 
   // * 유저데이터를 가져오기 위한 userDataAtom 사용
   const [userData, setUserData] = useRecoilState(userDataAtom);
   const [privateData, setPrivateData] = useRecoilState(privateDataAtom);
+
+  const myName = userData && userData.accountname ? userData.accountname : "";
 
   // * 로그아웃
   const handleSignOut = () => {
@@ -81,10 +83,13 @@ export default function Header() {
   return (
     <>
       <Headers>
+        {/* 뒤로 가기 버튼 */}
         <button type="button" onClick={() => navigate(-1)}>
           <img src={IconCherovonL} alt="뒤로 가기" />
         </button>
-        {pathname === "/home" && (
+
+        {/* 홈 */}
+        {pathname === "/" && (
           <>
             <p>Home</p>
             <button type="button" onClick={() => navigate("/search")}>
@@ -92,27 +97,45 @@ export default function Header() {
             </button>
           </>
         )}
+
+        {/* 뉴스레터 */}
         {pathname === "/newsletter" && <p>뉴스레터</p>}
+
+        {/* 게시물 작성 */}
         {pathname === "/post/upload" && <p>피드 업로드</p>}
-        {pathname === "/search" && (
-          <SearchInput type="text" placeholder="계정 검색" />
-        )}
-        {pathname === "/product/upload" && <p>상품 업로드</p>}
-        {pathname === "/chat/room" && (
-          <>
-            <p>상대방 이름</p>
-            <button type="button" onClick={() => navigate("/search")}>
-              <img src={IconDots} alt="계정 검색" />
-            </button>
-          </>
-        )}
-        {pathname === `/profile/${account}` && (
+
+        {/* 프로필 */}
+        {/* 정규표현식으로 profile url 체크 */}
+        {pathname.match(
+          new RegExp(`^/profile/${myName}(/|/column/?|/grid/?)?$`),
+        ) && (
           <button type="button" onClick={e => setProfileModal(e)}>
             <img src={IconDots} alt="설정" />
           </button>
         )}
+
+        {/* 검색 */}
+        {pathname === "/search" && (
+          <SearchInput type="text" placeholder="계정 검색" />
+        )}
+
+        {/* 팔로워/팔로잉 */}
         {pathname === `/profile/${account}/following` && <p>Following</p>}
         {pathname === `/profile/${account}/follower` && <p>Followers</p>}
+
+        {/* 상품 작성 */}
+        {(pathname === "/product/upload" ||
+          pathname === `/product/${productId}/edit`) && <p>상품 업로드</p>}
+
+        {/* 채팅방 */}
+        {pathname === "/chat/room" && (
+          <>
+            <p>상대방 이름</p>
+            <button type="button" onClick={() => navigate("/search")}>
+              <img src={IconDots} alt="설정" />
+            </button>
+          </>
+        )}
       </Headers>
       <Outlet />
     </>
