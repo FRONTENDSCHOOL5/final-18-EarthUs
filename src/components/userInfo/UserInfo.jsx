7ca22986f4e6d4cd6949ce64b74678af5200eab8
Jay <sinaelj@gmail.com>
2023-06-20 14:32:30 +0900

    ✨ feat: 모달 조건부 랜더링 추가

--- src/components/userInfo/UserInfo.jsx
+++ src/components/userInfo/UserInfo.jsx
@@ -1,8 +1,14 @@
-import React from "react";
-import { useLocation } from "react-router-dom";
-import { useRecoilState } from "recoil";
+/* eslint-disable no-shadow */
+/* eslint-disable no-unused-vars */
+import React, { useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import { useQueryClient } from "@tanstack/react-query";
+import { useRecoilState, useSetRecoilState } from "recoil";
 
 import iconDots from "../../assets/images/dots.svg";
+import useApiMutation from "../../hooks/useApiMutation";
+import modalConfigState from "../../recoil/modalConfigAtom";
+import modalState from "../../recoil/modalStateAtom";
 import userDataAtom from "../../recoil/userDataAtom";
 import Avatar from "../common/avatar/Avatar";
 
@@ -16,19 +22,126 @@ export default function UserInfo({
   id,
   more,
   children,
-  handleModal,
   searchKeyword,
 }) {
-  const location = useLocation();
-
-  // * 유저데이터를 가져오기 위한 userDataAtom 사용
+  // * 전역 상태 관리를 위한 Recoil State 가져오기
   const [userData] = useRecoilState(userDataAtom);
-  // eslint-disable-next-line no-underscore-dangle
-  // const myName = userData ? userData.accountname.trim().toLowerCase() : "";
+  const setModalOpen = useSetRecoilState(modalState);
+  const setModalConfig = useSetRecoilState(modalConfigState);
   const myName =
     userData && userData.accountname
       ? userData.accountname.trim().toLowerCase()
       : "";
+
+  const location = useLocation();
+  const navigate = useNavigate();
+  const queryClient = useQueryClient();
+  const currentUser = account.trim().toLowerCase();
+
+  // * 피드아이템 삭제
+  const [deleteFeed, setDeleteFeed] = useState(null);
+  // eslint-disable-next-line no-unused-vars
+  const deleteFeedMutation = useApiMutation(
+    deleteFeed,
+    "DELETE",
+    {},
+    {
+      onSuccess: () => {
+        // eslint-disable-next-line no-console
+        console.log("게시물이 삭제되었습니다.");
+        queryClient.invalidateQueries(`/profile/${account}`);
+      },
+    },
+  );
+
+  const handleDeleteFeed = postId => {
+    const url = `/post/${postId}`;
+    setDeleteFeed(url);
+    deleteFeedMutation.mutate();
+  };
+
+  // * 신고하기 API 호출
+  const [reports, setReports] = useState(null);
+  const setReportMutation = useApiMutation(
+    reports,
+    "POST",
+    {},
+    {
+      onSuccess: () => {
+        // eslint-disable-next-line no-console
+        console.log("신고되었습니다.");
+      },
+    },
+  );
+
+  const handleReport = postId => {
+    const url = `/post/${postId}/report`;
+    setReports(url);
+    setReportMutation.mutate();
+  };
+
+  // * 신고하기 모달
+  const reportConfirm = e => {
+    e.stopPropagation();
+    setModalConfig({
+      type: "confirm",
+      title: "게시물을 신고하시겠어요?",
+      buttons: [
+        {
+          label: "취소",
+          onClick: e => {
+            e.stopPropagation();
+            setModalOpen(false);
+          },
+        },
+        {
+          label: "신고하기",
+          onClick: () => handleReport(id),
+        },
+      ],
+    });
+    setModalOpen(true);
+  };
+
+  // * 수정/삭제 모달
+  const deleteConfirm = e => {
+    e.stopPropagation();
+    setModalConfig({
+      type: "confirm",
+      title: "파일을 삭제하시겠어요?",
+      buttons: [
+        {
+          label: "취소",
+          onClick: e => {
+            e.stopPropagation();
+            setModalOpen(false);
+          },
+        },
+        {
+          label: "삭제",
+          onClick: () => handleDeleteFeed(id),
+        },
+      ],
+    });
+    setModalOpen(true);
+  };
+  const deleteCardModal = e => {
+    e.stopPropagation();
+    setModalConfig({
+      type: "bottomSheet",
+      buttons: [
+        {
+          label: "삭제",
+          onClick: e => deleteConfirm(e),
+        },
+        {
+          label: "수정",
+          onClick: () => navigate(`/post/${id}/edit`),
+        },
+      ],
+    });
+    setModalOpen(true);
+  };
 
   // Link 랜더링 조건부 출력
   function renderLinkContent() {
@@ -45,6 +158,7 @@ export default function UserInfo({
           <Avatar profileImg={profileImg} size={40} />
           <div>
             <strong
+              // eslint-disable-next-line react/no-danger
               dangerouslySetInnerHTML={{ __html: userNameWithHighlight }}
             />
             {id ? <p>@{account}</p> : ""}
@@ -67,7 +181,7 @@ export default function UserInfo({
 
   return (
     <Users isSearch={location.pathname === "/search"}>
-      {account.trim().toLowerCase() !== myName ? (
+      {currentUser !== myName ? (
         <UserHeader to={`/profile/${account}`}>
           {renderLinkContent()}
         </UserHeader>
@@ -79,7 +193,15 @@ export default function UserInfo({
       )}
       {children}
       {more ? (
-        <button type="button" onClick={handleModal}>
+        <button
+          type="button"
+          // onClick={handleModal}
+          onClick={
+            currentUser === myName
+              ? e => deleteCardModal(e)
+              : e => reportConfirm(e)
+          }
+        >
           <img src={iconDots} alt="더 보기" />
         </button>
       ) : (
