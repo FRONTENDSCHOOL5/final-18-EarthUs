b1787c716acdfbfb0167fcc6cc4cd145ee0a7d19
Jay <sinaelj@gmail.com>
2023-06-21 12:46:55 +0900

    ✨ feat: TextArea 공통 컴포넌트 추가

---
+++ src/components/common/textArea/textarea.style.js
@@ -0,0 +1,33 @@
+import styled from "styled-components";
+
+const TextAreaWrap = styled.textarea`
+  width: 100%;
+  outline: none;
+  border: none;
+  overflow: hidden;
+  resize: none;
+  white-space: "pre-wrap";
+  padding: 0.5rem;
+  border-bottom: solid var(--color-dark) 2px;
+  font: inherit;
+  letter-spacing: inherit;
+  max-height: calc(100vh - var(--size-header) - var(--size-tabBar) - 30rem);
+
+  &::placeholder {
+    color: var(--color-dark);
+    font-size: var(--font-size-md);
+  }
+
+  // 값이 입력되거나 포커스 상태일 때
+  &.filled,
+  &:focus {
+    border-bottom: 2px solid var(--color-primary);
+  }
+
+  // 에러 클래스가 되었을 때
+  &.error {
+    border-bottom-color: var(--color-error);
+  }
+`;
+
+export default TextAreaWrap;
