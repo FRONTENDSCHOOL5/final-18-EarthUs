a36b33b651e89e503496e8d9d5d0f3d79e2b92bf
Jay <sinaelj@gmail.com>
2023-06-21 11:55:56 +0900

    🐎 ci: configuration Url 추가

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,2 +1,21 @@
-const BASE_URL = "https://api.mandarin.weniv.co.kr";
-export default BASE_URL;
+export const BASE_URL = "https://api.mandarin.weniv.co.kr";
+export const NO_IMAGE = "https://api.mandarin.weniv.co.kr/1687097552358.png";
+
+/**
+ * @Profile
+ * const { account } = useParams();
+ * const PROFILE_DETAIL = getProfileDetailPath(account);
+ * const PROFILE_UPLOAD = getProfileUploadPath(account);
+ * const PROFILE_EDIT = getProfileEditPath(account);
+ */
+export const getProfileDetailPath = account => `/profile/${account}`;
+export const getProfileUploadPath = account => `/profile/${account}/upload`;
+export const getProfileEditPath = account => `/profile/${account}/edit`;
+
+/**
+ * @Post
+ * const { postId } = useParams();
+ * const POST_EDIT = getPostEditPath(postId);
+ */
+export const POST_UPLOAD = `/post/upload`;
+export const getPostEditPath = postId => `/post/${postId}/edit`;
