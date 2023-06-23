d1f7538259f9ca75c3ea2b0349a075a16bed7153
Jay <sinaelj@gmail.com>
2023-06-23 15:10:45 +0900

    ✨ feat: 무한스크롤 추가

--- src/components/profileFeed/ProfileFeed.jsx
+++ src/components/profileFeed/ProfileFeed.jsx
@@ -1,23 +1,71 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-shadow */
-import React, { useState } from "react";
-import { useNavigate, Link, useParams } from "react-router-dom";
-import { useQueryClient } from "@tanstack/react-query";
-import { useRecoilState, useSetRecoilState } from "recoil";
+import React from "react";
+import InfiniteScroll from "react-infinite-scroller";
+import { Link, useParams } from "react-router-dom";
+import { v4 as uuidv4 } from "uuid";
 
 import grid from "../../assets/images/grid-view.svg";
 import layer from "../../assets/images/layer-image.svg";
 import column from "../../assets/images/spred-view.svg";
-import useApiMutation from "../../hooks/useApiMutation";
-import useApiQuery from "../../hooks/useApiQuery";
-import modalConfigState from "../../recoil/modalConfigAtom";
-import modalState from "../../recoil/modalStateAtom";
-import userDataAtom from "../../recoil/userDataAtom";
+import useApiInfiniteQuery from "../../hooks/useApiInfiniteQuery";
 import A11yHidden from "../common/a11yHidden/A11yHidden";
 import BreakLine from "../common/breakLine/BreakLine";
 import Card from "../common/card/Card";
 
 import { ProfileFeedWrap, ViewBtn, FeedView } from "./profileFeed.style";
+
+export default function ProfileFeed() {
+  const { account, view } = useParams();
+  const {
+    data: feedData,
+    hasNextPage: feedHasNextPage,
+    fetchNextPage: feedFetchNextPage,
+  } = useApiInfiniteQuery(`/post/${account}/userpost`, "post");
+
+  return (
+    <InfiniteScroll
+      hasMore={feedHasNextPage}
+      loadMore={() => feedFetchNextPage()}
+    >
+      <ProfileFeedWrap>
+        {feedData && (
+          <>
+            <header>
+              <h2>
+                <A11yHidden>피드</A11yHidden>
+              </h2>
+
+              <ViewBtn to={`/profile/${account}/column`} icon={column}>
+                <A11yHidden>1열 보기</A11yHidden>
+              </ViewBtn>
+              <ViewBtn to={`/profile/${account}/grid`} icon={grid}>
+                <A11yHidden>3열 보기</A11yHidden>
+              </ViewBtn>
+            </header>
+
+            {/* view 값에 따라 1열 또는 3열로 표기 */}
+            <FeedView view={view}>
+              {feedData.pages.map(page => {
+                return (
+                  <React.Fragment key={uuidv4()}>
+                    {page.post.map(item =>
+                      view === "grid" ? (
+                        <GridView key={item.id} item={item} layer={layer} />
+                      ) : (
+                        <ColumnView key={item.id} item={item} />
+                      ),
+                    )}
+                  </React.Fragment>
+                );
+              })}
+            </FeedView>
+          </>
+        )}
+      </ProfileFeedWrap>
+    </InfiniteScroll>
+  );
+}
 
 function GridView({ item }) {
   const { id, image } = item;
@@ -51,184 +99,26 @@ function ColumnView({ item }) {
     content,
     author,
     heartCount,
-    account,
+    hearted,
     commentCount,
     createdAt,
-    hearted,
   } = item;
   const time = createdAt.slice(0, 10).split("-");
-  const navigate = useNavigate();
-  const queryClient = useQueryClient();
-
-  // * 전역 상태 관리를 위한 Recoil State 가져오기
-  const setModalOpen = useSetRecoilState(modalState);
-  const setModalConfig = useSetRecoilState(modalConfigState);
-
-  const [deleteFeed, setDeleteFeed] = useState(null);
-  const [userData] = useRecoilState(userDataAtom);
-  const myName = userData ? userData.accountname.trim().toLowerCase() : "";
-
-  // * 피드아이템 삭제
-  // eslint-disable-next-line no-unused-vars
-  const deleteFeedMutation = useApiMutation(
-    deleteFeed,
-    "DELETE",
-    {},
-    {
-      onSuccess: () => {
-        // eslint-disable-next-line no-console
-        console.log("게시물이 삭제되었습니다.");
-        queryClient.invalidateQueries(`/profile/${account}`);
-      },
-    },
-  );
-
-  const handleDeleteFeed = postId => {
-    const url = `/post/${postId}`;
-    setDeleteFeed(url);
-    deleteFeedMutation.mutate();
-  };
-
-  // * 신고하기 API 호출
-  const [reports, setReports] = useState(null);
-  const setReportMutation = useApiMutation(
-    reports,
-    "POST",
-    {},
-    {
-      onSuccess: () => {
-        // eslint-disable-next-line no-console
-        console.log("신고되었습니다.");
-      },
-    },
-  );
-
-  const handleReport = postId => {
-    const url = `/post/${postId}/report`;
-    setReports(url);
-    setReportMutation.mutate();
-  };
-
-  // * 신고하기 모달
-  const reportConfirm = e => {
-    e.stopPropagation();
-    setModalConfig({
-      type: "confirm",
-      title: "게시물을 신고하시겠어요?",
-      buttons: [
-        {
-          label: "취소",
-          onClick: e => {
-            e.stopPropagation();
-            setModalOpen(false);
-          },
-        },
-        {
-          label: "신고하기",
-          onClick: () => handleReport(id),
-        },
-      ],
-    });
-    setModalOpen(true);
-  };
-
-  // * 수정/삭제 모달
-  const deleteConfirm = e => {
-    e.stopPropagation();
-    setModalConfig({
-      type: "confirm",
-      title: "파일을 삭제하시겠어요?",
-      buttons: [
-        {
-          label: "취소",
-          onClick: e => {
-            e.stopPropagation();
-            setModalOpen(false);
-          },
-        },
-        {
-          label: "삭제",
-          onClick: () => handleDeleteFeed(id),
-        },
-      ],
-    });
-    setModalOpen(true);
-  };
-  const deleteCardModal = e => {
-    e.stopPropagation();
-    setModalConfig({
-      type: "bottomSheet",
-      buttons: [
-        {
-          label: "삭제",
-          onClick: e => deleteConfirm(e),
-        },
-        {
-          label: "수정",
-          onClick: () => navigate(`/post/${id}/edit`),
-        },
-      ],
-    });
-    setModalOpen(true);
-  };
-
   return (
     <Card
       key={id}
+      id={id}
       accountname={author.accountname}
       profileImage={author.image}
       username={author.username}
       postImage={image}
       heartCount={heartCount}
       commentCount={commentCount}
-      id={id}
       time={`${time[0]}년 ${time[1]}월 ${time[2]}일`}
-      handleModal={
-        author.accountname.trim().toLowerCase() === myName
-          ? e => deleteCardModal(e)
-          : e => reportConfirm(e)
-      }
+      hearted={hearted}
       postID={id}
-      hearted
     >
       <BreakLine content={content} />
     </Card>
-  );
-}
-
-export default function ProfileFeed() {
-  const { account, view } = useParams();
-  const { data } = useApiQuery(`/post/${account}/userpost`, "get");
-
-  return (
-    <ProfileFeedWrap>
-      {data && (
-        <>
-          <header>
-            <h2>
-              <A11yHidden>피드</A11yHidden>
-            </h2>
-
-            <ViewBtn to={`/profile/${account}/column`} icon={column}>
-              <A11yHidden>1열 보기</A11yHidden>
-            </ViewBtn>
-            <ViewBtn to={`/profile/${account}/grid`} icon={grid}>
-              <A11yHidden>3열 보기</A11yHidden>
-            </ViewBtn>
-          </header>
-
-          {/* view 값에 따라 1열 또는 3열로 표기 */}
-          <FeedView view={view}>
-            {data.post.map(item =>
-              view === "grid" ? (
-                <GridView key={item.id} item={item} layer={layer} />
-              ) : (
-                <ColumnView key={item.id} item={item} />
-              ),
-            )}
-          </FeedView>
-        </>
-      )}
-    </ProfileFeedWrap>
   );
 }
