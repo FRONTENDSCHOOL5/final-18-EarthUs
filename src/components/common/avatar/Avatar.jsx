5506f5c527b35550c95f8a6ac81d3e8d957c8750
jisooyoon <yoonmallang@gmail.com>
2023-06-23 17:16:17 +0900

    🦄 refactor: 이미지 에러 함수 분리

--- src/components/common/avatar/Avatar.jsx
+++ src/components/common/avatar/Avatar.jsx
@@ -5,18 +5,17 @@ import { NO_PROFILE_IMAGE } from "../../../utils/config";
 import { Avatars, Img } from "./avatar.style";
 
 export default function Avatar({ profileImg, size }) {
+  // * 이미지가 로드되지 않았을 때 onError 이벤트 핸들러 실행
+  const handleImgError = e => {
+    e.target.onerror = null;
+    e.target.src = NO_PROFILE_IMAGE;
+    e.target.style.width = "24px";
+    e.target.style.height = "24px";
+  };
+
   return (
     <Avatars size={size}>
-      <Img
-        src={profileImg}
-        alt="프로필 이미지"
-        onError={e => {
-          e.target.onerror = null;
-          e.target.src = NO_PROFILE_IMAGE;
-          e.target.style.width = "24px";
-          e.target.style.height = "24px";
-        }}
-      />
+      <Img src={profileImg} alt="프로필 이미지" onError={handleImgError} />
     </Avatars>
   );
 }
