4a1cf0a1b5b604f9983ebea885c28c528dbcab6e
jisooyoon <yoonmallang@gmail.com>
2023-06-23 11:35:46 +0900

    🐞 fix: 서버에 없는 프로필 이미지일 경우 대체 이미지 적용

--- src/components/common/avatar/Avatar.jsx
+++ src/components/common/avatar/Avatar.jsx
@@ -1,11 +1,22 @@
 import React from "react";
 
+import { NO_PROFILE_IMAGE } from "../../../utils/config";
+
 import { Avatars, Img } from "./avatar.style";
 
 export default function Avatar({ profileImg, size }) {
   return (
     <Avatars size={size}>
-      <Img src={profileImg} alt="프로필 이미지" />
+      <Img
+        src={profileImg}
+        alt="프로필 이미지"
+        onError={e => {
+          e.target.onerror = null;
+          e.target.src = NO_PROFILE_IMAGE;
+          e.target.style.width = "24px";
+          e.target.style.height = "24px";
+        }}
+      />
     </Avatars>
   );
 }
