8a7d8587922308077c4c3a2f5bc6e1ab4a89afb6
jisooyoon <yoonmallang@gmail.com>
2023-06-23 10:55:52 +0900

    🦄 refactor: 프로필 이미지 컴포넌트로 변경

--- src/components/common/avatar/Avatar.jsx
+++ src/components/common/avatar/Avatar.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-import Avatars from "./avatar.style";
+import { Avatars, Img } from "./avatar.style";
 
 export default function Avatar({ profileImg, size }) {
   return (
     <Avatars size={size}>
-      <img src={profileImg} alt="프로필 이미지" />
+      <Img src={profileImg} alt="프로필 이미지" />
     </Avatars>
   );
 }
