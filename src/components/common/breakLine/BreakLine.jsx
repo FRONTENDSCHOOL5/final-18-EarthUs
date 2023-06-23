9e6bf7deb4fd3581b085f077e0cef3cf5375061e
jisooyoon <yoonmallang@gmail.com>
2023-06-23 11:00:35 +0900

    🦄 refactor: 불필요한 태그 제거

--- src/components/common/breakLine/BreakLine.jsx
+++ src/components/common/breakLine/BreakLine.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 
 export default function BreakLine({ content }) {
-  const formattedContent = content.split("\n").map((line, index) => (
-    // eslint-disable-next-line react/no-array-index-key
-    <React.Fragment key={index}>
-      {line}
-      <br />
-    </React.Fragment>
-  ));
-  return <span>{formattedContent}</span>;
+  return (
+    <>
+      {content.split("\n").map((line, index) => (
+        // eslint-disable-next-line react/no-array-index-key
+        <React.Fragment key={index}>
+          {line}
+          <br />
+        </React.Fragment>
+      ))}
+    </>
+  );
 }
