87a3f48d91d6a42620c96dfcdba96b6d1e7f3934
Jay <sinaelj@gmail.com>
2023-06-24 18:01:32 +0900

    ✨ feat: 게시물 등록페이지 모듈패턴 적용

---
+++ src/pages/post/postUpload/PostImageField.jsx
@@ -0,0 +1,141 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-console */
+/* eslint-disable no-alert */
+import React, { useState, useEffect } from "react";
+import { useLocation, useParams } from "react-router-dom";
+import { v4 as uuidv4 } from "uuid";
+
+import A11yHidden from "../../../components/common/a11yHidden/A11yHidden";
+import ImagePrev from "../../../components/common/imagePrev/ImagePrev";
+import Input from "../../../components/common/input/Input";
+import useApiQuery from "../../../hooks/useApiQuery";
+import useImgMutationHook from "../../../hooks/useImageUploader";
+
+import { InputPrevWrap, ImgLabel, ImgUploadButton } from "./postUpload.style";
+
+const PostImageField = React.memo(function PostImageField({
+  editImageData,
+  setEditImageData,
+  POST_EDIT,
+}) {
+  const { pathname } = useLocation();
+  const { postId } = useParams();
+
+  const [apiImg, setApiImg] = useState([]);
+
+  // * 게시물 수정 페이지일 경우, 기존 유저 정보 불러오기
+  const { data: apiData } = useApiQuery(
+    `/post/${postId}`,
+    "get",
+    {},
+    pathname === POST_EDIT,
+  );
+  useEffect(() => {
+    if (apiData) {
+      if (apiData && apiData.post && apiData.post.image) {
+        setApiImg(apiData.post.image.split(","));
+      }
+    }
+  }, [apiData]);
+
+  // * 이미지 업로드 Hook 호출
+  const { mutation: uploadPostImage, image } =
+    useImgMutationHook("/image/uploadfiles");
+
+  // * 이미지 업로드
+  const handleImgChange = e => {
+    const imageFile = e.target.files[0];
+    const imageFiles = Array.from(e.target.files);
+
+    // 취소 눌렀을 경우 업로드 실행 중단
+    if (e.target.files.length === 0) {
+      return;
+    }
+
+    // 이미지 파일 크기 제한
+    if (imageFiles.some(file => file.size > 10000000)) {
+      alert("10MB 이하 이미지를 넣어주세요");
+      return;
+    }
+
+    // 이미지 파일 개수 제한
+    if (imageFiles.length > 3) {
+      alert("3개 이하의 파일을 업로드 하세요");
+      return;
+    }
+    // 이미지 파일 확장자명 제한
+    const fileNamesplitedArr = imageFile.name.split(".");
+    const fileExtension = fileNamesplitedArr[fileNamesplitedArr.length - 1];
+    const fileExtensions = ["jpg", "gif", "png", "jpeg", "bmp", "tif", "heic"];
+
+    if (
+      imageFiles.some(
+        file => !fileExtensions.includes(file.name.split(".").pop()),
+      )
+    ) {
+      alert("이미지 파일만 업로드 가능합니다.");
+      return;
+    }
+    uploadPostImage.mutate(imageFiles);
+  };
+
+  // 이미지 업로드 성공 시 데이터 저장
+  useEffect(() => {
+    if (image) {
+      setApiImg(image.split(","));
+      setEditImageData(image);
+    }
+  }, [image, pathname]);
+
+  // * 이미지 삭제
+  const handleDelete = selectImgFile => {
+    setApiImg(apiImg.filter(img => img !== selectImgFile));
+    setEditImageData(image);
+    console.log(editImageData);
+  };
+
+  return (
+    <>
+      <InputPrevWrap>
+        {apiImg.length > 0 &&
+          apiImg.map((imgUrl, idx) => (
+            <ImagePrev
+              // eslint-disable-next-line react/no-array-index-key
+              key={uuidv4()}
+              src={imgUrl}
+              alt={`${idx + 1}번째 이미지`}
+              event={() => handleDelete(imgUrl)}
+            />
+          ))}
+      </InputPrevWrap>
+      <Input
+        className="itemImage"
+        type="file"
+        id="itemImage"
+        onChange={e => handleImgChange(e)}
+        multiple
+        data={!apiImg.length ? "data" : ""}
+        tabIndex={-1}
+      >
+        <A11yHidden>이미지 등록</A11yHidden>
+        {/* 이미지 등록 */}
+        <ImgLabel data={!apiImg.length ? "data" : ""}>
+          {/* 접근성 */}
+          <ImgUploadButton
+            data={apiImg.length < 1 ? "data" : ""}
+            role="button"
+            tabIndex={0}
+            onKeyDown={e => {
+              if (e.key === " " || e.key === "Enter") {
+                e.preventDefault();
+                e.target.click();
+              }
+            }}
+          />
+        </ImgLabel>
+      </Input>
+    </>
+  );
+});
+
+export default PostImageField;
--- src/pages/post/postUpload/PostUpload.jsx
+++ src/pages/post/postUpload/PostUpload.jsx
@@ -1,44 +1,59 @@
 /* eslint-disable no-unused-expressions */
-/* eslint-disable react/jsx-props-no-spreading */
 /* eslint-disable no-console */
-/* eslint-disable no-alert */
 /* eslint-disable no-unused-vars */
 import React, { useEffect, useState } from "react";
 import { useLocation, useParams, useNavigate } from "react-router-dom";
-// eslint-disable-next-line import/no-extraneous-dependencies
-import { v4 as uuidv4 } from "uuid";
 
 import A11yHidden from "../../../components/common/a11yHidden/A11yHidden";
 import Button from "../../../components/common/button/Button";
-import ImagePrev from "../../../components/common/imagePrev/ImagePrev";
-import Input from "../../../components/common/input/Input";
 import TextArea from "../../../components/common/textArea/TextArea";
 import useApiMutation from "../../../hooks/useApiMutation";
 import useApiQuery from "../../../hooks/useApiQuery";
-import useImageUploader from "../../../hooks/useImageUploader";
-import { getPostEditPath, POST_UPLOAD } from "../../../utils/config";
+import useValidations from "../../../hooks/useValidations";
+import {
+  getPostEditPath,
+  POST_UPLOAD,
+  NO_PROFILE_IMAGE,
+} from "../../../utils/config";
 
-import { InputPrevWrap, ImgLabel, ImgUploadButton } from "./postUpload.style";
+import PostImageField from "./PostImageField";
 
-export default function PostUpload() {
+export default function PostUpload({ profileImage, setProfileImage }) {
   const { pathname } = useLocation();
   const { postId } = useParams();
   const navigate = useNavigate();
   const POST_EDIT = getPostEditPath(postId);
 
-  // 데이터 패치
-  const [apiImg, setApiImg] = useState([]);
-  const [editImageData, setEditImageData] = useState("");
-
-  // 인풋필드 상태 저장
+  const [editImageData, setEditImageData] = useState(NO_PROFILE_IMAGE);
   const [disabledBtn, setDisabledBtn] = useState(pathname === POST_UPLOAD);
-  const [content, setContent] = useState("");
 
-  // 오류 메시지 상태 저장
-  const [contentError, setContentError] = useState("");
+  // * 게시물 유효성 검사
+  // 유효성 검사 객체 생성
+  const contentObj = useValidations(
+    "내용 또는 이미지를 입력해주세요.",
+    /.+/,
+    "",
+  );
+
+  // 유효성 검사 실행
+  const inputs = [contentObj];
+  const validationFields = () => {
+    let valid = true;
+    inputs.forEach(input => {
+      if (input.error || !input.value) {
+        valid = false;
+      }
+    });
+    setDisabledBtn(!valid);
+  };
+
+  // 유효성 검사 실행할 의존성 배열 지정
+  useEffect(() => {
+    validationFields();
+  }, [contentObj]);
 
   // * 게시물 수정 시 이전 정보 출력
-  const { data: apiData } = useApiQuery(
+  const { data } = useApiQuery(
     `/post/${postId}`,
     "get",
     {},
@@ -46,90 +61,32 @@ export default function PostUpload() {
   );
 
   useEffect(() => {
-    if (apiData) {
-      setApiImg(apiData.post.image.split(","));
-      setContent(apiData.post.content);
-      pathname === POST_EDIT && setEditImageData(apiData.post.image);
+    if (data) {
+      contentObj.validate(data.post.content);
+      pathname === POST_EDIT && setEditImageData(data.post.image);
     }
-  }, [apiData]);
-
-  // * 게시물 유효성 검사
-  const validationFields = e => {
-    const currentValue = e.target.value;
-    switch (e.target.id) {
-      case "content":
-        setContent(currentValue);
-        if (currentValue.length < 1) {
-          setContentError("내용 또는 이미지를 입력해주세요.");
-          setDisabledBtn(true);
-        } else {
-          setContentError("");
-          setDisabledBtn(false);
-        }
-        break;
-      default:
-        break;
-    }
-    return null;
-  };
-
-  // * 이미지 업로드 Hook 호출
-  const { mutation: uploadPostImage, image } =
-    useImageUploader("/image/uploadfiles");
-
-  // * 이미지 업로드
-  const handleImgChange = e => {
-    const imageFile = e.target.files[0];
-    const imageFiles = Array.from(e.target.files);
-
-    // 취소 눌렀을 경우 업로드 실행 중단
-    if (e.target.files.length === 0) {
-      return;
-    }
-
-    // 이미지 파일 크기 제한
-    if (imageFiles.some(file => file.size > 10000000)) {
-      alert("10MB 이하 이미지를 넣어주세요");
-      return;
-    }
-
-    // 이미지 파일 개수 제한
-    if (imageFiles.length > 3) {
-      alert("3개 이하의 파일을 업로드 하세요");
-      return;
-    }
-    // 이미지 파일 확장자명 제한
-    const fileNamesplitedArr = imageFile.name.split(".");
-    const fileExtension = fileNamesplitedArr[fileNamesplitedArr.length - 1];
-    const fileExtensions = ["jpg", "gif", "png", "jpeg", "bmp", "tif", "heic"];
-
-    if (
-      imageFiles.some(
-        file => !fileExtensions.includes(file.name.split(".").pop()),
-      )
-    ) {
-      alert("이미지 파일만 업로드 가능합니다.");
-      return;
-    }
-    uploadPostImage.mutate(imageFiles);
-  };
-
-  useEffect(() => {
-    if (image) {
-      setApiImg(image.split(","));
-      pathname === POST_EDIT && setEditImageData(image);
-    }
-  }, [image, pathname === POST_EDIT]);
+  }, [data]);
 
   // 이미지 수정 없이 게시글만 변경하는 경우 state 전달
   const editPostMutation = useApiMutation(
     `/post/${postId}`,
     "PUT",
-    { post: { content, image: image || editImageData } },
     {
-      onSuccess: data => {
+      post: {
+        content: contentObj.value,
+        image: editImageData,
+      },
+    },
+    {
+      onSuccess: ({ post }) => {
         console.log("게시물 수정이 완료되었습니다.");
-        navigate(`/profile/${data.post.author.accountname}`);
+        navigate(`/profile/${post.author.accountname}`);
+      },
+      onerror: error => {
+        console.warn(
+          "게시물 수정 요청에 실패했습니다.",
+          error.response.data.message,
+        );
       },
     },
   );
@@ -138,24 +95,26 @@ export default function PostUpload() {
   const uploadPostMutation = useApiMutation(
     "/post",
     "POST",
-    { post: { content, image } },
     {
-      onSuccess: data => {
+      post: {
+        content: contentObj.value,
+        image: editImageData,
+      },
+    },
+    {
+      onSuccess: ({ post }) => {
         console.log("게시물 등록이 완료되었습니다.");
-        navigate(`/profile/${data.post.author.accountname}`);
+        navigate(`/profile/${post.author.accountname}`);
       },
     },
   );
 
-  // * 이미지 삭제
-  const handleDelete = selectImgFile => {
-    setApiImg(apiImg.filter(img => img !== selectImgFile));
-  };
-
   // * 게시물 업로드
   const handleUploadPost = e => {
     e.preventDefault();
+    validationFields();
 
+    console.log("clicked");
     if (pathname === POST_UPLOAD) {
       uploadPostMutation.mutate();
     } else if (pathname === POST_EDIT) {
@@ -166,51 +125,20 @@ export default function PostUpload() {
   return (
     <section>
       <h2>
-        <A11yHidden>게시물 이미지 등록</A11yHidden>
+        <A11yHidden>게시물 작성</A11yHidden>
       </h2>
+      <PostImageField
+        POST_EDIT={POST_EDIT}
+        editImageData={editImageData}
+        setEditImageData={setEditImageData}
+      />
       <form onSubmit={handleUploadPost}>
-        <InputPrevWrap>
-          {apiImg.length > 0 &&
-            apiImg.map((imgUrl, idx) => (
-              <ImagePrev
-                // eslint-disable-next-line react/no-array-index-key
-                key={uuidv4()}
-                src={imgUrl}
-                alt={`${idx + 1}번째 이미지`}
-                event={() => handleDelete(imgUrl)}
-              />
-            ))}
-        </InputPrevWrap>
-        <Input
-          className="itemImage"
-          type="file"
-          id="itemImage"
-          onChange={e => handleImgChange(e)}
-          multiple
-          data={!apiImg.length ? "data" : ""}
-        >
-          {/* 이미지 등록 */}
-          <ImgLabel data={!apiImg.length ? "data" : ""}>
-            {/* 접근성 */}
-            <ImgUploadButton
-              data={apiImg.length < 1 ? "data" : ""}
-              role="button"
-              tabIndex={0}
-              onKeyDown={e => {
-                if (e.key === " " || e.key === "Enter") {
-                  e.preventDefault();
-                  e.target.click();
-                }
-              }}
-            />
-          </ImgLabel>
-        </Input>
         <TextArea
           type="text"
           id="content"
-          value={content}
-          error={contentError}
-          onChange={validationFields}
+          value={contentObj.value}
+          error={contentObj.error}
+          onChange={contentObj.onChange}
           label="내용"
           placeholder="내용을 입력하세요."
           required
@@ -218,7 +146,8 @@ export default function PostUpload() {
         <Button
           type="submit"
           size="cta"
-          variant={disabledBtn ? "disabled" : "primary"}
+          variant="primary"
+          disabled={disabledBtn && "disabled"}
         >
           {pathname === POST_UPLOAD ? "저장" : "수정"}
         </Button>
