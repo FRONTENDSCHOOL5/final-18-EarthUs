01971b68001ac1b635e0d1f98fc4463a53e3254f
jisooyoon <yoonmallang@gmail.com>
2023-06-24 23:58:15 +0900

    🦄 refactor: Blank 컴포넌트 onClick props 추가

--- src/components/blank/Blank.jsx
+++ src/components/blank/Blank.jsx
@@ -1,21 +1,14 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
 
 import Button from "../common/button/Button";
 
 import BlankWrap from "./blank.style";
 
-export default function Blank({ children, btn }) {
-  const navigate = useNavigate();
+export default function Blank({ children, btn, onClick }) {
   return (
     <BlankWrap>
       <p>{children}</p>
-      <Button
-        size="cta"
-        variant="primary"
-        type="button"
-        onClick={() => navigate("/search")}
-      >
+      <Button size="cta" variant="primary" type="button" onClick={onClick}>
         {btn}
       </Button>
     </BlankWrap>
