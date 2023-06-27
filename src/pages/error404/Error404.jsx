51cb5cef4bc78ce8d1847354a3203044b89c0ab7
jisooyoon <yoonmallang@gmail.com>
2023-06-27 10:40:33 +0900

    ✨ feat: 에러 페이지 로그인 유무에 따른 조건부 렌더링

--- src/pages/error404/Error404.jsx
+++ src/pages/error404/Error404.jsx
@@ -2,14 +2,21 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 
 import Blank from "../../components/blank/Blank";
-import { HOME } from "../../utils/config";
+import { SIGN_IN } from "../../utils/config";
 
 export default function Error404() {
   const navigate = useNavigate();
 
+  const token = localStorage.getItem("privateData");
+
   return (
-    <Blank btn="홈으로 이동" onClick={() => navigate(HOME)}>
-      페이지를 찾을 수 없습니다. :&#40;
+    <Blank
+      btn={token ? "이전 페이지으로 이동" : "로그인으로 이동"}
+      onClick={() => (token ? navigate(-1) : navigate(SIGN_IN))}
+    >
+      {token
+        ? "페이지를 찾을 수 없습니다. :("
+        : "로그인 이후 접근 가능한 페이지입니다. :("}
     </Blank>
   );
 }
