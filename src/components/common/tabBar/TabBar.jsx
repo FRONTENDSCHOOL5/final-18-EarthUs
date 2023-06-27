255ffc55d053d9dea8f6f3a2cc8fe46c11661692
jisooyoon <yoonmallang@gmail.com>
2023-06-27 17:16:08 +0900

    🦄 refactor: 스크린 리더 고려하여 alt 값 제거

--- src/components/common/tabBar/TabBar.jsx
+++ src/components/common/tabBar/TabBar.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable no-unused-vars */
 import React from "react";
 import { Outlet } from "react-router-dom";
 import { useRecoilState } from "recoil";
@@ -23,30 +22,30 @@ export default function TabBar() {
         <MenuList>
           <TabMenu>
             <Links to="/home">
-              <img src={iconHome} alt="홈" />홈
+              <img src={iconHome} alt="" />홈
             </Links>
           </TabMenu>
           <TabMenu>
             <Links to="/chat/list">
-              <img src={iconChat} alt="채팅" />
+              <img src={iconChat} alt="" />
               채팅
             </Links>
           </TabMenu>
           <TabMenu>
             <Links to="/newsletter">
-              <img src={iconNewsletter} alt="뉴스레터" />
+              <img src={iconNewsletter} alt="" />
               뉴스레터
             </Links>
           </TabMenu>
           <TabMenu>
             <Links to="/post/upload">
-              <img src={iconEdit} alt="게시물 작성" />
+              <img src={iconEdit} alt="" />
               게시물 작성
             </Links>
           </TabMenu>
           <TabMenu>
             <Links to={`/profile/${myName}`}>
-              <img src={iconUser} alt="프로필" />
+              <img src={iconUser} alt="" />
               프로필
             </Links>
           </TabMenu>
