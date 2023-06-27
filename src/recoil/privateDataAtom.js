a3d2b62e64fcec4b690ea6fe4482847e9bceb45c
jisooyoon <yoonmallang@gmail.com>
2023-06-27 10:50:10 +0900

    🦄 refactor: 로컬스토리지 디폴트값 null로 수정

--- src/recoil/privateDataAtom.js
+++ src/recoil/privateDataAtom.js
@@ -1,14 +1,8 @@
 import { atom } from "recoil";
 
 // API를 호출하는 대상의 비공개 데이터를 저장하는 atom
-const userDataFromLocalStorage = JSON.parse(
-  localStorage.getItem("privateData"),
-) || {
-  key: "privateDataAtom",
-  default: {
-    token: "",
-  },
-};
+const userDataFromLocalStorage =
+  JSON.parse(localStorage.getItem("privateData")) || null;
 
 const privateState = atom({
   key: "privateState",
--- src/recoil/userDataAtom.js
+++ src/recoil/userDataAtom.js
@@ -1,20 +1,8 @@
 import { atom } from "recoil";
 
 // API를 호출하는 대상의 데이터를 저장하는 atom
-const userDataFromLocalStorage = JSON.parse(
-  localStorage.getItem("userData"),
-) || {
-  key: "userDataAtom",
-  default: {
-    username: "",
-    accountname: "",
-    email: "",
-    image: "",
-    intro: "",
-    token: "",
-    id: "",
-  },
-};
+const userDataFromLocalStorage =
+  JSON.parse(localStorage.getItem("userData")) || null;
 
 const loginState = atom({
   key: "loginState",
