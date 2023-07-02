381eaab2707f436844cb8b80eb1b15219147b675
Jay <sinaelj@gmail.com>
2023-07-03 00:40:24 +0900

    🌈 style: figcaption 숨김처리 해제

--- src/styles/GlobalStyle.jsx
+++ src/styles/GlobalStyle.jsx
@@ -151,17 +151,6 @@ const GlobalStyle = createGlobalStyle`
   input:-webkit-autofill:active {
     transition: background-color 5000s ease-in-out 0s;
   }
-
-  figcaption {
-  clip: rect(1px, 1px, 1px, 1px);
-  clip-path: inset(50%);
-  width: 1px;
-  height: 1px;
-  margin: -1px;
-  overflow: hidden;
-  padding: 0;
-  position: absolute;
-  }
 `;
 
 export default GlobalStyle;
